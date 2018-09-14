29a1627ecdeb238378338e0f1f5f2d265e6f6479
github username <github email address>
2018-09-14 14:55:23 -0500

    add logout feature specs

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     sequence(:name) { |n| "test#{n}" }
     sequence(:email) { |n| "test#{n}@test.com" }
-    password '1234567890'
-    password_confirmation '1234567890'
+    password { '1234567890' }
+    password_confirmation { '1234567890' }
   end
 end
