0802b176209e329609a438d75e3b3fd9c063747f
github username <github email address>
2018-09-14 14:18:18 -0500

    add a users factory

---
+++ spec/factories/users.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :user do
+    sequence(:name) { |n| "test#{n}" }
+    sequence(:email) { |n| "test#{n}@test.com" }
+    password '1234567890'
+    password_confirmation '1234567890'
+  end
+end
