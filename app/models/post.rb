a7fcdbd87c763f0f7c6ba8a216aa2d590b1a7996
github username <github email address>
2018-09-18 14:09:34 -0500

    Add a spec for the Post model's default_scope

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,5 @@ class Post < ApplicationRecord
   belongs_to :user
   belongs_to :category
 
-  default_scope -> { include(:user).order(created: :desc) }
+  default_scope -> { includes(:user).order(created_at: :desc) }
 end
