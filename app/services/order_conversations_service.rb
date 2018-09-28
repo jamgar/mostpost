baaba4aa4ac29aa85c2cdc3f95218d9dddd7bf8e
github username <github email address>
2018-09-28 14:54:34 -0500

    Get data for group conversations in ApplicationController - Update the opened_conversations_windows method - Update the OrderConversationsService

--- app/services/order_conversations_service.rb
+++ app/services/order_conversations_service.rb
@@ -8,8 +8,12 @@ class OrderConversationsService
   def call
     all_private_conversations = Private::Conversation.all_by_user(@user.id)
                                                      .includes(:messages)
-    return all_conversations = all_private_conversations.sort{ |a, b|
-      b.messages.last.created_at.to_i <=> a.messages.last.created_at.to_i
+    all_group_conversations = @user.group_conversations.includes(:messages)
+    all_conversations = all_private_conversations + all_group_conversations
+
+    return all_conversations = all_conversations.sort{ |a, b| 
+      b.messages.last.created_at <=> a.messages.last.created_at
     }
   end
+
 end
